baf0e2bcebb04e4267ea539a3c1c39d60da7b1c6
Hywel Rees <hjr555@gmail.com>
2024-10-01 20:47:25 +0100

    Fix print text

--- main.asm
+++ main.asm
@@ -7,20 +7,16 @@
     INCLUDE "constants/memory.asm"
 
 string_copyright
-    defb _at, 22, 1, paper, white, copyri, " 2024 Hywel Rees", 0
+    defb copyri, " 2024 Hywel Rees", 0
 
 start:
-    call ROM_CLS
-    ld hl, DF_SZ    ; Address of bottom two lines
-    ld a, 0
-    ld (hl), a      ; Set to zero
     ld hl, string_copyright
 
 print_char:
     ld a, (hl)
     cp 0            ; Compare A to zero
     jr z, exit      ; Jump if a = 0 (end of string)
-    call ROM_PRINT
+    RST $10         ; print
     inc hl          ; move pointer to next index on string
     jr print_char
 
